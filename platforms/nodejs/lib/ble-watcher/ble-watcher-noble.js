154fc2794bf3080b1e6f0cd29861dcd8e8a48112
Paul Varache <paul@kano.me>
2018-03-12 15:37:48 +0000

    Add support for hidden addresses on macOS

--- platforms/nodejs/lib/ble-watcher/ble-watcher-noble.js
+++ platforms/nodejs/lib/ble-watcher/ble-watcher-noble.js
@@ -5,6 +5,11 @@ class NobleWatcher extends NobleWatcherBase {
     constructor() {
         super(noble);
     }
+    onDiscovered(peripheral) {
+        // Can't cache device per address. See: https://github.com/noble/noble/blob/master/README.md#peripheral-discovered
+        // Will apply the test function on all advertisements
+        this.emit('discover', peripheral);
+    }
 }
 
 module.exports = NobleWatcher;
